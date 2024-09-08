e3b33d87fa56cb35bbba460a4e8c25e7cbc2fac9
Alexander K <niktonezapomnil@gmail.com>
2024-09-09 02:06:42 +0500

    init

---
+++ js/scripts.js
@@ -0,0 +1,104 @@
+let scrollTop = $(window).scrollTop();
+
+$(window).scroll(function (evt) {
+    scrollTop = $(this).scrollTop();
+});
+
+$(document).ready(function () {
+    // анимация меню
+    $('.menu').click(function (e) {
+        e.preventDefault();
+        (this.classList.contains('active') === true) ? this.classList.remove('active'): this.classList.add('active');
+
+        $('.header').toggleClass('active');
+        $('body').on('click', function (e) {
+            let div = $('.menu-links-wrapper, .menu');
+
+            if (!div.is(e.target) && div.has(e.target).length === 0) {
+                $('.header, .menu').removeClass('active');
+            }
+        });
+    });
+
+    // якоря для ссылок
+    $('.anchor[href^="#"]').click(function () {
+        $('.header').removeClass('active');
+        $('.menu').removeClass('active');
+
+        elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top - 150;
+        $('html, body').animate({
+            scrollTop: destination
+        }, 500, 'swing');
+        return false;
+    });
+
+    // аккордеон
+    function openAccordion() {
+        var wrap = $('.accordion-wrap');
+        var accordion = wrap.find('.accordion-title');
+
+        accordion.on('click', function () {
+            var $this = $(this);
+            var $parent = $(this).parent();
+            var content = $this.next();
+
+            if (content.is(':visible')) {
+                $this.removeClass('active');
+                $parent.removeClass('active');
+                content.slideUp('fast');
+            } else {
+                $this.addClass('active');
+                $parent.addClass('active');
+                content.slideDown('fast');
+            }
+
+        });
+    }
+    openAccordion();
+
+    // открытие модалок
+    $('body').on('click','.js-open-modal', function(e){
+        e.preventDefault();
+        let id = $(this).attr('href');
+        $.fancybox.open({
+            src: id,
+            type: 'inline'
+        });
+    });
+
+    // маски
+    if ($('.phone-mask').length) {
+        $('.phone-mask').inputmask({
+            mask: "+79999999999",
+            "clearIncomplete": true
+        });
+    }
+
+    // select2
+    if($('.select').length > 1) {
+        $('select').each(function() {
+            let $this = $(this).not('.select-search');
+            let parent = $(this).not('.select-search').parents('.select');
+            $this.select2({
+                minimumResultsForSearch: Infinity,
+                dropdownParent: parent
+            });
+        });
+        $('.select-search').each(function() {
+            let $this = $(this);
+            let parent = $(this).parents('.select');
+            $this.select2({
+                dropdownParent: parent
+            });
+        });
+    } else if($('.select').length == 1) {
+        $('select').select2({
+            minimumResultsForSearch: Infinity,
+            dropdownParent: $('.select')
+        });
+        $('.select-search').select2({
+            dropdownParent: $('.select')
+        });
+    }
+});
