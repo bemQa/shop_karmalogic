f3ad3fd450d978ef5e5e28be4b7f80a21097878a
Alexander K <niktonezapomnil@gmail.com>
2024-09-10 18:57:20 +0500

    upd

--- js/scripts.js
+++ js/scripts.js
@@ -105,6 +105,7 @@ $(document).ready(function () {
     // sliders
     const courses_cards = new Swiper('.courses-cards', {
         slidesPerView: 'auto',
+        loop: true,
         freeMode: true
     });
 });
