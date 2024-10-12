0be870967d0c3a60b1de95b8705a0eaa2780c19f
Alexander K <niktonezapomnil@gmail.com>
2024-10-13 01:45:33 +0500

    mousemove parallax

--- js/scripts.js
+++ js/scripts.js
@@ -507,3 +507,15 @@ $(document).ready(function () {
         });
     }
 });
+
+// mousemove parallax
+document.addEventListener("mousemove", parallax);
+function parallax(event) {
+    this.querySelectorAll(".mouse-parallax-element").forEach((shift) => {
+        const position = shift.getAttribute("value");
+        const x = (window.innerWidth - event.pageX * position) / 90;
+        const y = (window.innerHeight - event.pageY * position) / 90;
+
+        shift.style.transform = `translateX(${x}rem) translateY(${y}rem)`;
+    });
+}
